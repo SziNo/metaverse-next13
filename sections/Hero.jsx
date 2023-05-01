d88517fa11a39303ac8303614696c40c0ac6eb0e
SziNo <szino1848@gmail.com>
2023-05-01 16:23:57 +0200

    remove link tag

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -41,17 +41,15 @@ const Hero = () => (
             fill
           />
         </div>
-        <a href='#explore'>
-          <div className='w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
-            <Image
-              src='/stamp.png'
-              alt='stamp'
-              className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain'
-              width={155}
-              height={155}
-            />
-          </div>
-        </a>
+        <div className='w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10'>
+          <Image
+            src='/stamp.png'
+            alt='stamp'
+            className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain'
+            width={155}
+            height={155}
+          />
+        </div>
       </motion.div>
     </motion.div>
   </section>
